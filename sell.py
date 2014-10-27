02357fc7383808339aa959fbbab621fba18b408b
Yash Mehrotra <yashmehrotra95@gmail.com>
2014-10-27 06:21:13 +0530

    Taking names from txt file

--- sell.py
+++ sell.py
@@ -19,41 +19,60 @@ class BookawaySellTest(unittest.TestCase):
         self.assertIn("Sell", driver.title)
 
         driver.maximize_window()
+        
+        my_dic = {'seller_name':None}
+        input_list = []
 
-        name = driver.find_element_by_id("name")
-        name.send_keys("Avijit Gupta")
+        seller_names = open('input/seller_names.txt','r').readlines()
+        seller_names = map(lambda each:each.strip('\n'),seller_names)
+
+        #input_list = [
+        #    'seller_name':seller_names
+        #]
+        for name in seller_names:
+            #my_dic.update({'seller_name':name}) # Which is better
+            my_dic['seller_name']=name
+            input_list.append({'seller_name':name})
+        # For input in input_list , input['seller_name']
+        pdb.set_trace() 
+        for inp in input_list:
+            name = driver.find_element_by_id("name")
+            #name.send_keys("Avijit Gupta")
+            name.send_keys(inp['seller_name'])
         
-        email = driver.find_element_by_id("email")
-        email.send_keys("526avijit@gmail.com")
+            email = driver.find_element_by_id("email")
+            email.send_keys("526avijit@gmail.com")
         
-        phone = driver.find_element_by_id("phone")
-        phone.send_keys("9971958437")
+            phone = driver.find_element_by_id("phone")
+            phone.send_keys("9971958437")
         
-        password = driver.find_element_by_id("password")
-        password.send_keys("Avijit Gupta")
+            password = driver.find_element_by_id("password")
+            password.send_keys("Avijit Gupta")
         
-        sell_clg = driver.find_element_by_id("sell-clg")
-        sell_clg.send_keys("JIIT Sector-62, Noida")
+            sell_clg = driver.find_element_by_id("sell-clg")
+            sell_clg.send_keys("JIIT Sector-62, Noida")
         
-        # Make it better , this approach maybe wrong
-        category = driver.find_element_by_id("subject")
-        select_cat = [ cat for cat in category.find_elements_by_tag_name('option') if cat.text == 'Mathematics']
-        select_cat[0].click()
+            # Make it better , this approach maybe wrong
+            category = driver.find_element_by_id("subject")
+            select_cat = [ cat for cat in category.find_elements_by_tag_name('option') if cat.text == 'Mathematics']
+            select_cat[0].click()
         
-        book = driver.find_element_by_id("book")
-        book.send_keys("Puzzles to Puzzle You")
+            book = driver.find_element_by_id("book")
+            book.send_keys("Puzzles to Puzzle You")
         
-        author = driver.find_element_by_id("author")
-        author.send_keys("Shakuntla Devi")
+            author = driver.find_element_by_id("author")
+            author.send_keys("Shakuntla Devi")
         
-        s_cost = driver.find_element_by_id("s-cost")
-        s_cost.send_keys("100")
+            s_cost = driver.find_element_by_id("s-cost")
+            s_cost.send_keys("100")
         
-        r_cost = driver.find_element_by_id("r-cost")
-        r_cost.send_keys("100")
+            r_cost = driver.find_element_by_id("r-cost")
+            r_cost.send_keys("100")
         
-        driver.find_element_by_id("new-btn").click()
-        # submit.send_keys(Keys.RETURN)
+            driver.find_element_by_id("new-btn").click()
+            # submit.send_keys(Keys.RETURN)
+
+            driver.refresh()
 
     def tearDown(self):
         self.driver.close()
